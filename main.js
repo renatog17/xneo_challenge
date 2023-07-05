94030f04688b98e1885984fd410f6d23e3e71895
Renato Oliveira <renatog.oliveira17@gmail.com>
2023-07-05 00:58:00 -0300

    form created

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-
 const btn_add = document.getElementById('btn-add')
+const form = document.getElementById('form')
+
+console.log(form)
+
 btn_add.addEventListener('click', (e)=>{
     e.preventDefault()
     console.log('botão clicado')
+    form.style.display = 'block'
 })
